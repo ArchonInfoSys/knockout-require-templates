14fda7083c45939d7c922ccf1b672caf3799df2c
Chad Lee <git@chadly.net>
2013-03-10 13:34:56 -0500

    Removing global configuration support and adding support for r.js optimizer.

--- template.js
+++ template.js
@@ -1,51 +1,41 @@
-﻿/*globals define */
-define(["text", "knockout", "stringTemplateEngine"], function (text, ko) {
-   var inferTemplateName = function(name) {
-      var templateName,
-      index = name.indexOf("!"),
-      parts;
-      
+﻿define(["require", "text", "knockout", "stringTemplateEngine"], function (require, text, ko) {
+   var loader = {
+      load: function (name, req, load, config) {
+         var onLoad = config.isBuild ? load : function (content) {
+            var safeName = inferTemplateName(name);
+            ko.templates[safeName] = content;
+            load(content);
+         };
+
+         text.load(name, req, onLoad, config);
+      },
+      write: function (pluginName, moduleName, write, config) {
+         text.write("text", moduleName, write, config);
+
+         var safeName = inferTemplateName(moduleName);
+
+         write.asModule(pluginName + "!" + moduleName,
+            "define(['text!" + moduleName + "', 'knockout', 'stringTemplateEngine'], function (content, ko) {" +
+            "ko.templates['" + safeName + "'] = content;" +
+            "});\n");
+      }
+   };
+
+   function inferTemplateName(name) {
+      var templateName;
+
+      var index = name.indexOf("!");
       if (index !== -1) {
          //use the template name that is specified
          templateName = name.substring(index + 1, name.length);
-      }
-      else {
+      } else {
          //use the file name sans the path as the template name
-         parts = name.split("/");
+         var parts = name.split("/");
          templateName = parts[parts.length - 1].split(".").join("-");
       }
 
       return templateName;
-   },
-   loader = {
-      load: function (name, req, load, config) {
-         var onLoad = function (content) {
-            var safeName = inferTemplateName(name);
-            ko.templates[safeName] = content;
-            load(content);
-         },
-         templateName = name,
-         index;
-         
-         //use the global configuration if it is defined
-         if (config !== undefined && config.loadKoTemplate !== undefined) {
-            if (config.loadKoTemplate.templatePath !== undefined) {
-               templateName = config.loadKoTemplate.templatePath + templateName;
-            }
-            if (config.loadKoTemplate.extension !== undefined) {
-               
-               //if we have a ! in the path we only want the text before it
-               index = templateName.indexOf("!");
-               if (index !== -1) {
-                  templateName = templateName.substring(0, index);
-               }
-               templateName = templateName + config.loadKoTemplate.extension;
-            }
-         }
-
-         text.load(templateName, req, onLoad, config);
-      }
-   };
+   }
 
    return loader;
 });
